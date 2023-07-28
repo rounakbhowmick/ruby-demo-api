b953689ba717060b5b1d8021c4e5b1014aace52c
rounak-yubi <rounak.bhowmick@go-yubi.com>
2023-07-28 13:39:37 +0530

    added controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      # get 'products/index'
+      # get 'products/show'
+      # get 'products/create'
+      resources :products, only[:index, :show, :create]
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
